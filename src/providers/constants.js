63034f25b0511e17450459015217fced71a13c63
Pash <pashkarapetyan.ait@gmail.ru>
2022-07-22 23:38:59 +0400

    Fix host to remote server

--- src/providers/constants.js
+++ src/providers/constants.js
@@ -1,5 +1,2 @@
 export const COURSES = 2;
-export const HOST =
-  process.env.NODE_ENV === "production"
-    ? "http://176.9.170.51:4954"
-    : "http://192.168.16.222:3000";
+export const HOST = "http://176.9.170.51:4954";
